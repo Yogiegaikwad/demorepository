3063d9148e46d9db6861dfecc3f6d114b68b7c52
Yogita Gaikwad <yogitag273@gmail.com>
2025-11-10 03:58:54 +0530

    Two numbers Addition code added in demo.c

---
+++ demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, sum;
+    printf("\n Enter first number: ");
+    scanf("\n%d",&n1);
+
+    printf("\n Enter second number: ");
+    scanf("\n%d",&n2);
+
+    sum = n1 + n2;
+
+    printf("\n Sum is: ",sum);
+
+    return 0;
+
+}
